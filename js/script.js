9abb2f1d335514ce63c15632be29bd5cb9719fc3
fahrumfahriansyah <fahrumfahriansyah1@gmail.com>
2022-09-16 00:54:13 +0700

    menambahkan data for

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector('.btn')
 const container = document.querySelector('.container')
 const container1 = document.querySelector('.container1')
+const list1nya = Array.from(document.querySelectorAll(".list1nya"))
+
+list1nya.forEach((a, b) => {
+    const data = [...a.textContent].map(a => { return '<span>' + a + '</span>' }).join('')
+    list1nya[b].innerHTML = data
+})
 btn.addEventListener('click', function () {
     const clas = container.classList
     const clasMain = container1.classList
